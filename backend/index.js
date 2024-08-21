2279b6e3e537ec80553a53caa86e2299e5745175
Sahan2000 <udanasahan1@gmail.com>
2024-08-21 20:15:48 +0530

    done cart crud operation

--- backend/index.js
+++ backend/index.js
@@ -122,11 +122,50 @@ async function run() {
   })
 
     // Carts Routes
+
+    // add to cart
     app.post('/add-to-cart', async (req, res) => {
       const newCart = await req.body;
       const result = await cartCollection.insertOne(newCart);
       res.send(result);
     });
+
+    //get cart item by id
+    app.get('/cart-item/:id', async (req,res)=>{
+      const id = req.params.id;
+      const email = req.query.email;
+      const query = {
+        classId: id,
+        userMail: email
+      };
+      const projection = {classId: 1};
+      const result = await cartCollection.findOne(query,{projection: projection});
+      res.send(result);
+    });
+
+    //cart info by user email
+    app.get('/cart/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { userMail: email };
+      const projection = {classId: 1}
+      const carts = await cartCollection.find(query, {projection:projection}).toArray();
+      const classId = carts.map((cart)=> new ObjectId(cart.classId));
+      const query2 = {_id: {$in: classId}};
+      const result = await cartCollection.find(query2).toArray();
+      res.send(result);
+    });
+
+    // delete cart item
+    app.delete('/delete-cart-item/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {classId: id}
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Payment Routes
+
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
