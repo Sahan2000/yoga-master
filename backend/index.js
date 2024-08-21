3868038c1ed02b0984712f66e6ff04f3307a01a0
Sahan2000 <udanasahan1@gmail.com>
2024-08-21 21:27:08 +0530

    done payment routes

--- backend/index.js
+++ backend/index.js
@@ -166,8 +166,72 @@ async function run() {
     });
 
     // Payment Routes
-    
+    app.post("/create-payment-intent", async (req, res) =>{
+      const { price } = req.body;
+      const amount = parseInt(price) * 100;
 
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount:amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    // post payment info  to db
+    app.post('/payment-info', async (req,res) => {
+      const paymentInfo = req.body;
+      const classesId = paymentInfo.classesId;
+      const userEmail = paymentInfo.userEmail;
+      const singleClassId = req.query.classId;
+      let query;
+      if(singleClassId){
+        query = {classId: singleClassId, userMail: userEmail}; 
+      }else{
+        query = {classId: {$in: classesId}};
+      }
+
+      const classesQuery ={_id: {$in: classesId.map(id => new ObjectId(id))}};
+      const classes = await classesCollection.find(classesQuery).toArray();
+      const newEnrolledData = {
+        userEmail: userEmail,
+        classId: singleClassId.map(id => new ObjectId(id)),
+        transactionId: paymentInfo.transactionId
+      };
+
+      const updatedDoc = {
+        $set: {
+          totalEnrolled:classes.reduce((total, current) => total + current.totalEnrolled, 0) + 1 || 0,
+          availableSeats: classes.reduce((total,current) => total + current.availableSeats, 0) -1 || 0
+        }
+      };
+
+      const updateResult = await classesCollection.updateMany(classesQuery,updatedDoc,{upsert: true});
+      const enrolledResult = await enrolledCollection.insertOne(newEnrolledData);
+      const deletedResult = await cartCollection.deleteMany(query);
+      const paymentResult = await paymentCollection.insertOne(paymentInfo);
+
+      res.send({ updateResult,enrolledResult,deletedResult,paymentResult});
+    });
+
+    // Payment history
+    app.get("/payment-history/:email", async (req,res)=>{
+      const email = req.params.email;
+      const query = {userEmail: email};
+      const result = await paymentCollection.find(query).sort({date: -1}).toArray();
+      res.send(result);
+    });
+
+    // Payment history length
+    app.get("/payment-history/:email", async (req,res)=>{
+      const email = req.params.email;
+      const query = {userEmail: email};
+      const total = await paymentCollection.countDocuments(query);
+      res.send(total);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
